457d2e5bb9218f1319a0418e49dbabc09f406893
Sergei <143517331+SeregaSimba@users.noreply.github.com>
2025-01-16 19:07:08 +0300

    feat: fix event tracking errors

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,8 @@
+const EventEmitter = require('events');
+let resultEvent = new EventEmitter();
 const path = require('path');
 const fs = require('fs');
-const pathResult = path.join(__dirname, 'text.txt');
-const result = fs.createReadStream(pathResult);
+resultEvent = path.join(__dirname, 'text.txt');
+const result = fs.createReadStream(resultEvent);
 
 result.on('data', (ivent) => console.log(ivent.toString()));
